7b5a65ce0f4eae5477e4cb8a15bb334f6e7497a1
Nasyrov Ruslan <nasyrov-rus@yandex-team.ru>
2021-03-06 22:55:37 +0500

    fixes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
-
-const port = ""
 
 func main() {
 	mux := http.NewServeMux()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	fPort := strings.Join([]string{":", port}, "")
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -17,8 +25,8 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Server listening on port %s", port)
-	err := http.ListenAndServe(port, mux)
+	log.Printf("Server listening on port %s", fPort)
+	err := http.ListenAndServe(fPort, mux)
 
 	if err != nil {
 		log.Fatal(err)
---
+++ config/prod.exs
@@ -0,0 +1,1 @@
+http: [port: {:system, "PORT"}]
